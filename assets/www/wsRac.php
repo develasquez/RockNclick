ad8f7fc77a54bddb17527781fc4a7591ff3aa1d6
felipe velasquez castro <felipe.velasquezc@gmail.com>
2012-09-25 19:13:55 -0300

    order

--- assets/www/wsRac.php
+++ assets/www/wsRac.php
@@ -67,7 +67,7 @@ echo $myJson;
 function getListaVideos(){
 
  $link=Conectarse(); 
- $query = "select Post ,Video from Imagenes where Video <> '' ";
+ $query = "select Post ,Video from Imagenes where Video <> '' order by Post desc  ";
  $myJson = "";
  $result=mysql_query($query,$link); 
     $rows = array();
